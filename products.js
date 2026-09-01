f77d4fa61a00da286ac8a74c73f6fd91e31bf7db
bogdanxui13 <bogdan13bobik@gmail.com>
2026-09-01 22:04:00 +0400

    Update products.js

--- products.js
+++ products.js
@@ -1,1 +1,34 @@
-
+const products = [
+    {
+        id: 1,
+        name: "Black Edition",
+        price: 4990,
+        stock: 7,
+        image: "images/product1.jpg",
+        description: "Минималистичная модель в чёрном цвете."
+    },
+    {
+        id: 2,
+        name: "Purple Night",
+        price: 5490,
+        stock: 3,
+        image: "images/product2.jpg",
+        description: "Стильная модель с фиолетовыми оттенками."
+    },
+    {
+        id: 3,
+        name: "Pink Edition",
+        price: 3990,
+        stock: 12,
+        image: "images/product3.jpg",
+        description: "Яркая модель в розовом исполнении."
+    },
+    {
+        id: 4,
+        name: "Dark Premium",
+        price: 6990,
+        stock: 2,
+        image: "images/product4.jpg",
+        description: "Премиальная модель в тёмном исполнении."
+    }
+];
